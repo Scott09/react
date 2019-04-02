2bf6d5776fc1249ba3124f307dab70a0fbf2dccc
Scott Appleton <scottappleton09@gmail.com>
2019-04-02 16:24:04 -0700

    testing git

--- components/src/ApprovalCard.js
+++ components/src/ApprovalCard.js
@@ -6,6 +6,7 @@ const ApprovalCard = (props) => {
     return (
         <div className="ui card">
             <div className ="content">{props.children}</div>
+            
             <div className="extra content">
                 <div className ="ui two buttons">
                     <div className="ui basic green button">Approve</div>
